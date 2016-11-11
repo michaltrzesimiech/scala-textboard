cf0fca304c8b4498131483e0ad0f14e314a7ecab
M Trzesimiech <michal.trzesimiech@gmail.com>
2016-11-11 19:27:35 +0100

    Elaborated on first draft, builds OK

--- build.sbt
+++ build.sbt
@@ -11,12 +11,14 @@ libraryDependencies ++=  Seq(
 					  "com.typesafe.akka" 	 %%    "akka-stream" 						  % "2.4.12",
 					  "com.typesafe.akka" 	 %%    "akka-http-core" 					  % "2.4.11",
 					  "com.typesafe.akka" 	 %%    "akka-http-experimental" 			  % "2.4.11",
-                      "org.postgresql"       %     "postgresql"               			  % "9.4-1206-jdbc41",
                       "com.typesafe.slick"   %%    "slick"            	      			  % "3.1.1",
                       "com.typesafe.slick"   %%    "slick-hikaricp"           			  % "3.1.1",
+					  "com.github.nscala-time" %%  "nscala-time" 						  % "2.14.0",
                       "net.codingwell"       %%    "scala-guice"              			  % "4.1.0",
+                      "org.postgresql"       %     "postgresql"               			  % "9.4-1206-jdbc41",
                       "org.scalatest"        %%    "scalatest"    	          			  % "3.0.0"     %    "test",
                       "com.typesafe.akka"    %%    "akka-http-testkit"        			  % "2.4.11"	%    "test"
+
 )
 
 fork in run := true
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,20 +17,22 @@ import com.typesafe.config.ConfigFactory
 import scala.concurrent.{ ExecutionContextExecutor, Future }
 import scala.io._
 import scala.io.StdIn
+import spray.json._
 import spray.json.DefaultJsonProtocol
 
-case class Thread(threadId: Int, subject: String, createdAt: DateTime)
-case class Post(threadId: Int, postId: Int, secretIt: Int, email: String, pseudonym: String, createdAt: DateTime)
+case class Thread(threadId: Int, subject: String, createdAt: String)
+case class Post(threadId: Int, postId: Int, secretIt: Int, email: String, pseudonym: String, createdAt: String)
 
+/** Domain model */
 object TextboardDb {
   case class CreateThread(thread: Thread)
   case class CreatePost(post: Post)
   case class EditPost(post: Post)
   case class DeletePost(post: Post)
   case object ListAllThreads
+  case class OpenThread(thread: Thread)
 }
 
-/** [Temporary] orchestrates actor behavior in relation to objects */
 class TextboardDb extends Actor {
   import TextboardDb._
   var threads: Map[Thread, Post] = Map.empty
@@ -40,36 +42,49 @@ class TextboardDb extends Actor {
     case CreatePost(post)     => println("post created")
     case EditPost(post: Post) => println("post edited")
     case DeletePost(post)     => println("post deleted")
+    case ListAllThreads       => threads.toList
+    case OpenThread(thread)   => println("open a single thread by id")
   }
 }
 
 /** Pulls in all implicit conversions to build JSON format instances, both RootJsonReader and RootJsonWriter. */
 trait TextboardJsonProtocol extends DefaultJsonProtocol with SprayJsonSupport {
-  implicit val threadFormat = jsonFormat2(Thread)
-  implicit val postFormat = jsonFormat2(Post)
+  implicit val threadFormat = jsonFormat3(Thread)
+  implicit val postFormat = jsonFormat6(Post)
 }
 
-/** Utilizes Akka HTTP high-level API to define DSL routes and orchestrate flow */
-object WebService extends App with TextboardJsonProtocol with SprayJsonSupport {
+/** Core service. Invokes ActorSystem, materializes Actor, orchestrates DSL routes, binds to server, terminates server.  */
+object Service extends App with TextboardJsonProtocol with SprayJsonSupport {
+  import akka.pattern.ask
+  import akka.util.Timeout
+  import scala.concurrent.duration._
+  import scala.language.postfixOps
 
   implicit val system = ActorSystem("inviter")
   implicit val materializer = ActorMaterializer()
 
-  import system.dispatcher // execution context
-  implicit val executor = system.dispatcher
+  implicit val executionContext = system.dispatcher
+  implicit val timeout = Timeout(5 seconds)
 
-  import akka.pattern.ask
-  //  implicit val timeout = Timeout(5 seconds)
+  val inviter = system.actorOf(Props[TextboardDb], name = "inviter")
 
-  val inviter = system.actorOf(Props[InvitationDb], name = "inviter")
-
-  /**
-   *  TODO: Set DSL routes, bind with server session
-   *  Inbound: HttpRequest outbound: Future[HttpResponse]
-   */
+  /** TODO: Set DSL routes least strict to most strict */
 
   val routes: Route = {
-    ...    
+    complete("OK")
+    //		  POST /thread
+    //		  POST /thread/:id/posts
+    //		  PUT /posts/:secret_id
+    //		  DELETE /posts/:secret_id
+    //		  GET /threads?limit=x&offset=x
+    //		  GET /thread/:thread_id/posts
   }
-  
+
+  val binding = Http().bindAndHandle(routes, "localhost", 8080)
+
+  println(s"Server running. Press any key to stop."); StdIn.readLine()
+  binding
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
 }  
