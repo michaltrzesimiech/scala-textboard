a1e51352f9a28cadbb3902bc9209f214a74159f0
M Trzesimiech <michal.trzesimiech@gmail.com>
2016-11-17 22:08:05 +0100

    Fixed tests

--- src/test/scala/service/RouteTest.scala
+++ src/test/scala/service/RouteTest.scala
@@ -4,13 +4,16 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.server._
 import Directives._
 
-import service._
+import service.TextboardDb._
+import service.TextboardJsonProtocol
+import service.Universe._
+import service.TextboardRoutes
 
 class TestRoutes extends WordSpec with Matchers with ScalatestRouteTest {
 
   "The service" should {
     "list all threads with success" in {
-      Get("threads") ~> route ~> check {
+      Get("/threads") ~> TextboardRoutes.route ~> check {
         status.isSuccess() shouldEqual true
       }
     }
@@ -18,15 +21,7 @@ class TestRoutes extends WordSpec with Matchers with ScalatestRouteTest {
 
   "The service" should {
     "post new thread with success" in {
-      Post("threads") ~> route ~> check {
-        status.isSuccess() shouldEqual true
-      }
-    }
-  }
-
-  "The service" should {
-    "post reply with success" in {
-      Post("threads" / "reply") ~> route ~> check {
+      Post("/threads") ~> TextboardRoutes.route ~> check {
         status.isSuccess() shouldEqual true
       }
     }
